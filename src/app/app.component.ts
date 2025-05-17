f3a6accea244258a500fe5a8a7ed164ff2ba48af
taektu_coding <taektu.coding@gmail.com>
2025-05-17 15:24:58 +0200

    added: header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IconSpriteComponent } from './icon-sprite/icon-sprite.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IconSpriteComponent, NavbarComponent],
+  imports: [
+    RouterOutlet,
+    IconSpriteComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
