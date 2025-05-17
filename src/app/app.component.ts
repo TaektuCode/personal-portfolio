1ed4ae126e9b457dd30b5dfcc951fb5458a7373a
taektu_coding <taektu.coding@gmail.com>
2025-05-17 15:07:20 +0200

    project setup + navbar setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconSpriteComponent } from './icon-sprite/icon-sprite.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconSpriteComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio2025';
---
+++ src/app/icon-sprite/icon-sprite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-sprite',
+  imports: [],
+  templateUrl: './icon-sprite.component.html',
+  styleUrl: './icon-sprite.component.scss'
+})
+export class IconSpriteComponent {
+
+}
