83db8bc18d89680fe0fd7a72006679a8f8e7b7c2
taektu_coding <taektu.coding@gmail.com>
2025-05-22 13:48:55 +0200

    added: language switcher in menu-dialog

--- src/app/navbar/menu-dialog/menu-dialog.component.ts
+++ src/app/navbar/menu-dialog/menu-dialog.component.ts
@@ -1,18 +1,60 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
-import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+  LangChangeEvent,
+} from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-menu-dialog',
-  imports: [TranslatePipe, TranslateDirective],
+  imports: [CommonModule, TranslatePipe, TranslateDirective],
   templateUrl: './menu-dialog.component.html',
-  styleUrls: ['./menu-dialog.component.scss'],
+  styleUrls: ['./menu-dialog.component.scss'], //
 })
-export class MenuDialogComponent {
+export class MenuDialogComponent implements OnInit, OnDestroy {
   @Output() closeDialogEvent = new EventEmitter<void>();
-
   @Input() isOpen = false;
+
+  currentLang: string;
+  private langChangeSubscription!: Subscription;
+
+  constructor(public translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || this.translate.defaultLang;
+  }
+
+  ngOnInit(): void {
+    this.langChangeSubscription = this.translate.onLangChange.subscribe(
+      (event: LangChangeEvent) => {
+        this.currentLang = event.lang;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    if (this.langChangeSubscription) {
+      this.langChangeSubscription.unsubscribe();
+    }
+  }
 
   onCloseButtonClick(): void {
     this.closeDialogEvent.emit();
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
+
+  isLanguageActive(lang: string): boolean {
+    return this.currentLang === lang;
   }
 }
