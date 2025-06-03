0cb3037027b8f9478f46fc02b8eb5d6b8b99974e
taektu_coding <taektu.coding@gmail.com>
2025-06-03 22:49:03 +0200

    added: skills.json to render skills icon + name in skills section

--- src/app/skill-set-section/skill-set-section.component.ts
+++ src/app/skill-set-section/skill-set-section.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+export interface Skill {
+  id: number | string;
+  name: string;
+  iconId: string;
+}
 
 @Component({
   selector: 'app-skill-set-section',
   imports: [],
   templateUrl: './skill-set-section.component.html',
-  styleUrl: './skill-set-section.component.scss'
+  styleUrls: ['./skill-set-section.component.scss'],
 })
-export class SkillSetSectionComponent {
+export class SkillSetSectionComponent implements OnInit {
+  public skills: Skill[] = [];
+  private skillsDataUrl = 'assets/data/skills.json';
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadSkills();
+  }
+
+  private loadSkills(): void {
+    this.http.get<Skill[]>(this.skillsDataUrl).subscribe({
+      next: (data) => {
+        this.skills = data;
+        console.log('Skills erfolgreich geladen:', this.skills);
+      },
+      error: (error) => {
+        console.error('Fehler beim Laden der Skill-Daten:', error);
+      },
+    });
+  }
 }
