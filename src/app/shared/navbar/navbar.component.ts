ef5f23dc1dd4dddea20f3a609bc46394b51a807c
taektu_coding <taektu.coding@gmail.com>
2025-06-23 06:05:45 +0200

    feat(header): Implement responsive navigation with mobile dialog and desktop menu

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -4,11 +4,16 @@ import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { MenuDialogComponent } from './menu-dialog/menu-dialog.component';
+import {
+  TranslatePipe,
+  TranslateService,
+  LangChangeEvent,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MenuDialogComponent],
+  imports: [CommonModule, MenuDialogComponent, TranslatePipe],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
@@ -16,26 +21,39 @@ export class NavbarComponent implements OnInit, OnDestroy {
   public useAlternateLogoColor = false;
   private routerSubscription!: Subscription;
 
+  public currentLang: string;
+  private langChangeSubscription!: Subscription;
+
   public isDialogVisible = false;
   public isDialogOpen = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, public translate: TranslateService) {
+    this.currentLang = this.translate.currentLang || this.translate.defaultLang;
+  }
 
   ngOnInit(): void {
     this.routerSubscription = this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: any) => {
         const darkLogoPages = ['/legal-notice', '/privacy-policy'];
-
         this.useAlternateLogoColor = darkLogoPages.includes(
           event.urlAfterRedirects
         );
       });
+
+    this.langChangeSubscription = this.translate.onLangChange.subscribe(
+      (event: LangChangeEvent) => {
+        this.currentLang = event.lang;
+      }
+    );
   }
 
   ngOnDestroy(): void {
     if (this.routerSubscription) {
       this.routerSubscription.unsubscribe();
+    }
+    if (this.langChangeSubscription) {
+      this.langChangeSubscription.unsubscribe();
     }
   }
 
@@ -55,5 +73,13 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
   closeDialog(): void {
     this.toggleDialog();
+  }
+
+  changeLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
+
+  isLanguageActive(lang: string): boolean {
+    return this.currentLang === lang;
   }
 }
